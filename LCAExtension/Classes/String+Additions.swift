35e6b611c406d0bfc4d42d8145a060a3ad718bd2
changan931 <931985308@qq.com>
2017-10-19 20:46:44 +0800

    Emoji

--- LCAExtension/Classes/String+Additions.swift
+++ LCAExtension/Classes/String+Additions.swift
@@ -23,4 +23,23 @@ public extension String {
         let size = (self as NSString).boundingRect(with: size, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: arributes, context: nil).size
         return size
     }
+    //MARK:是否含有Emoji表情
+    //https://coderwall.com/p/adlwew/check-if-swift-string-contains-an-emoji-or-dingbat-charater
+    var lca_containsEmoji: Bool {
+        for scalar in unicodeScalars {
+            switch scalar.value {
+            case 0x1F600...0x1F64F, // Emoticons
+            0x1F300...0x1F5FF, // Misc Symbols and Pictographs
+            0x1F680...0x1F6FF, // Transport and Map
+            0x2600...0x26FF,   // Misc symbols
+            0x2700...0x27BF,   // Dingbats
+            0xFE00...0xFE0F:   // Variation Selectors
+                return true
+            default:
+                continue
+            }
+        }
+        return false
+    }
+
 }
